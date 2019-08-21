7363ef239a94c54a9c0f0cf5d5e3e78cedbaa870
Romain GRIFFIER <romain.griffier@chu-bordeaux.fr>
2019-08-21 16:13:39 +0200

    Minor bug correction

--- R/Update.R
+++ R/Update.R
@@ -1,8 +1,13 @@
 #' @title Update the statsBordeaux package
 #' @description Update the statsBordeaux package
+#' @param force a boolean vector of length one. If TRUE, force installation, even if the
+#' remote state has not changed since the previous install. Default to FALSE
 #' @export
 #' @examples
 #' updateStatsBordeaux()
-updateStatsBordeaux <- function(){
-  devtools::install_github("rgriffier/statsBordeaux")
+updateStatsBordeaux <- function(force = FALSE){
+  if(!is.vector(force) | !is.logical(force) | length(force) != 1){
+    stop("force must be a boolean vector of length one.")
+  }
+  devtools::install_github("rgriffier/statsBordeaux", force = force)
 }
